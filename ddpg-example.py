8e2fe010b21c93531a0f74377ad204cd51116f37
JianShuiHuang <47372304+JianShuiHuang@users.noreply.github.com>
2020-09-06 10:22:11 +0800

    Update ddpg-example.py

--- ddpg-example.py
+++ ddpg-example.py
@@ -42,7 +42,7 @@ class ReplayMemory:
         ## TODO ##
         transitions = random.sample(self.buffer, batch_size)
         transition = []
-        for item in transitions:
+        for item in zip(*transitions):
             transition.append(torch.tensor(item, dtype=torch.float, device=device))
         
         return transition
